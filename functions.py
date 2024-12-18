00baff6530bc06c52b15690d272a760abd2e5a5d
Joel A <joelallencaliste@gmail.com>
2024-12-18 16:57:21 +0000

    refactored enter_credentials_to_website

--- functions.py
+++ functions.py
@@ -2,6 +2,7 @@
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 import os
 
@@ -16,8 +17,15 @@ def get_credentials_from_env():
 
 def enter_credentials_to_website(credential_list: list):
     """Fills out username and password form"""
-    driver = driver.get("http://www.google.com")
     
-    element = driver.find_element(By.ID, "passwd-id")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get("https://radius.mathnasium.com/Account/Login")
+    
+    username_box = driver.find_element(By.ID, "UserName")
+    username_box.send_keys(credential_list[0])
+    password_box = driver.find_element(By.ID, "Password")
+    password_box.send_keys(credential_list[1])
+    submit_button =  driver.find_element(By.ID, "login")
+    submit_button.submit()
     
     
