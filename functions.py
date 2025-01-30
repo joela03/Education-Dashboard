03fa8a6cf9a094fbb506fd3f9333d8c4a6b2b4ea
Joel A <joelallencaliste@gmail.com>
2025-01-30 14:05:51 +0000

    made extract_date function

--- functions.py
+++ functions.py
@@ -4,7 +4,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from datetime import datetime
 import os
+import time
 from dotenv import load_dotenv
 
 
@@ -33,39 +35,54 @@ def enter_credentials_to_website(credential_list: list):
     submit_button =  driver.find_element(By.ID, "login")
     submit_button.submit()
     
-    return driver
+    try:
+        bell = driver.find_element(By.ID, "bell")
+        if bell:
+            return driver
+    except:
+        driver.quit()
+        raise ValueError("Incorrect Submission")
     
-def check_successful_login(driver):
-    """Check's that login is successful via url"""
     
-    bell = driver.find_element(By.ID, "bell")
-    if bell:
-        return driver
-    else:
-        return ValueError("Incorrect Sumbmission")
+# def find_account_by_search(account_name: str, driver):
+#     """Searches and directs to parent account on Radius"""
     
-def find_account_by_search(account_name: str, driver):
-    """Searches and directs to parent account on Radius"""
+#     driver.get("https://radius.mathnasium.com/CustomerAccount")
     
-    search_bar = driver.find_element(By.ID, "ContactSearch")
-    search_bar.send_keys(account_name)
-    search_button = driver.find_element(By.ID, "globalbtnsearch")
-    search_button.submit()
-    href = driver.find_element_by_css_selector("linker").get_attribute('href')
-    driver.get(f"https://radius.mathnasium.com{href}")
+#     search_bar = driver.find_element(By.ID, "AccountNameSearch")
+#     search_bar.send_keys(account_name)
+#     search_button = driver.find_element(By.ID, "btnsearch")
+#     search_button.submit()
+#     driver.findElement(By.linktext).get_attribute('href').click()
+    
+#     return driver
+
+# def verify_valid_account_found(driver, account_name):
+#     """Verifies that the right account was found"""
+    
+#     h2_element = driver.find_element(By.XPATH, "//h2[text()='Jen Waugh']")
+#     h2_text = h2_element.text
+    
+#     if h2_text.strip() == account_name.strip():
+#         return driver
+#     else:
+#         return NameError("Invalid account found.")
+    
+
+def download_reports_tab(driver):
+    """Directs to reports tab from index"""
+    
+    driver.get("https://radius.mathnasium.com/StudentAttendanceMonthlyReport")
     
     return driver
 
-def verify_valid_account_found(driver, account_name):
-    """Verifies that the right account was found"""
-    
-    h2_element = driver.find_element(By.XPATH, "//h2[text()='Jen Waugh']")
-    h2_text = h2_element.text
-    
-    if h2_text.strip() == account_name.strip():
-        return driver
-    else:
-        return NameError("Invalid account found.")
+def extract_date(driver):
+    """Extracts date"""
+    time.sleep(3)
+
+    date_input = driver.find_element(By.ID, "ReportEnd")
+    date_value = date_input.get_attribute("value")
+    print(type(date_value))
     
     
-    
+        
--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import time
-from functions import (get_credentials_from_env, enter_credentials_to_website,
-                       check_successful_login, find_account_by_search,
-                       verify_valid_account_found)
+from functions import (get_credentials_from_env, enter_credentials_to_website, 
+                       download_reports_tab)
 
 if __name__ == "__main__":
     credential_list = get_credentials_from_env()
     driver = enter_credentials_to_website(credential_list)
-    driver = check_successful_login(driver)
-    driver = find_account_by_search("Eve Waugh", driver)
+    # driver = find_account_by_search("Eve Waugh", driver)
+    driver = download_reports_tab(driver)
     
     
