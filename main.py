f815f7638a8a1bd91db4bac56b070f9a574638a8
joela03 <joelallencaliste@gmail.com>
2025-03-02 23:33:42 +0000

    added to main function

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import time
 from functions import (get_credentials_from_env, enter_credentials_to_website, 
-                       download_reports)
+                       download_reports, filter_by_last_assessment, scrape_table,
+                       convert_col_to_dt)
 
 if __name__ == "__main__":
     credential_list = get_credentials_from_env()
     driver = enter_credentials_to_website(credential_list)
-    # driver = find_account_by_search("Eve Waugh", driver)
     driver = download_reports(driver)
-    
+    df = scrape_table(driver, "gridStudentReport")
+    filtered_df = filter_by_last_assessment(df, "Progress Check", 28, False)
+    filtered_df = convert_col_to_dt(filtered_df, ["Last\nProgress Check", "Last\nAssessment", "Last\nAttendance", "Last\nLP Update", "Last\nPR Sent"])
     
