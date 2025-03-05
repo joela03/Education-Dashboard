62e26b59bb905248fd1b5df7531e1a0bd2267118
Joel A <joelallencaliste@gmail.com>
2025-03-05 14:07:36 +0000

    configure front end in next js

---
+++ dashboard/src/components/app-sidebar.tsx
@@ -0,0 +1,90 @@
+import * as React from "react"
+
+import { SearchForm } from "@/components/search-form"
+import { VersionSwitcher } from "@/components/version-switcher"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail,
+} from "@/components/ui/sidebar"
+
+// This is sample data.
+const data = {
+  versions: ["1.0.1", "1.1.0-alpha", "2.0.0-beta1"],
+  navMain: [
+    {
+      title: "Education Management",
+      url: "#",
+      items: [
+        {
+          title: "Progress Check Status",
+          url: "/dashboard/edu/progress-check",
+        },
+        {
+          title: "Check-Up Status",
+          url: "/dashboard/edu/check-up",
+        },
+        {
+          title: "Plan Pace",
+          url: "/dashboard/edu/plan-pace",
+        },
+      ],
+    },
+    {
+      title: "At Risk",
+      url: "/dashboard/risk",
+      items: [
+        {
+          title: "General",
+          url: "/dashboard/risk/general",
+        },
+        {
+          title: "Attendance",
+          url: "/dashboard/risk/pace",
+          isActive: true,
+        }
+      ],
+    },
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar {...props}>
+      <SidebarHeader>
+        <VersionSwitcher
+          versions={data.versions}
+          defaultVersion={data.versions[0]}
+        />
+        <SearchForm />
+      </SidebarHeader>
+      <SidebarContent>
+        {/* We create a SidebarGroup for each parent. */}
+        {data.navMain.map((item) => (
+          <SidebarGroup key={item.title}>
+            <SidebarGroupLabel>{item.title}</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {item.items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild isActive={item.isActive}>
+                      <a href={item.url}>{item.title}</a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        ))}
+      </SidebarContent>
+      <SidebarRail />
+    </Sidebar>
+  )
+}
