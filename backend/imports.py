ce9e0a9f7dc35be18881bb5e657b659a427b957a
joela03 <joelallencaliste@gmail.com>
2025-03-09 16:49:58 +0000

    refactored to pass test

--- backend/imports.py
+++ backend/imports.py
@@ -7,12 +7,17 @@ import psycopg2.extras
 # Database connection
 def get_db_connection():
     "Sets up connection with database"
+
     # Load environment variables
     load_dotenv()
     db_host = os.getenv("DB_HOST")
     db_user = os.getenv("DB_USER")
     db_name = os.getenv("DB_NAME")
     db_port = os.getenv("DB_PORT")
+
+    # Check if all environment variables
+    if not all([db_host, db_user, db_name, db_port]):
+        raise ValueError("Missing required environment variables for DB connection.")
     
     # Establish and return the connection
     return psycopg2.connect(host=db_host, user=db_user, dbname=db_name, port=db_port)
