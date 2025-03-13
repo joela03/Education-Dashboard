a4676133109e5727b15bf60ef19abe7c93d17730
joela03 <joelallencaliste@gmail.com>
2025-03-13 00:18:45 +0000

    made api route to get students with lagging attendance

--- backend/database_functions.py
+++ backend/database_functions.py
@@ -2,8 +2,27 @@
 
 import psycopg2
 from psycopg2.extensions import connection
+from imports import (get_cursor, get_db_connection)
 
-def get_connection() -> connection:
-    """Sets up connection"""
-    return psycopg2.connect(
-        "dbname=mathnasium user=joela0 host=localhost")
+def get_student_attendance():
+    """Queries the database for students that haven't attended"""
+
+    conn = get_db_connection()
+    curs = get_cursor(conn)
+
+    curs.execute("""SELECT si.name, es.enrolment_status, si.mathnasium_id,
+                si.student_link, a.account_name, a.account_link,
+                ses.last_attendance, ses.attendance_count
+                FROM student_information as si
+                LEFT JOIN student_accounts AS sa ON si.student_id = sa.student_id
+                LEFT JOIN accounts AS a ON sa.account_id = a.account_id
+                LEFT JOIN student_education_stats AS ses ON si.student_id = ses.student_id
+                LEFT JOIN enrolment_status AS es ON si.enrolment_id = es.enrolment_id
+                WHERE ses.attendance_count < 5
+                AND ses.last_attendance < CURRENT_DATE - INTERVAL '7 days'
+                ;""")
+    data = curs.fetchall()
+    curs.close()
+    conn.close()
+
+    return data
