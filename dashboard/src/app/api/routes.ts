bcd42d6b3be2deca136a06b002f55725e165c8cb
Joel A <joelallencaliste@gmail.com>
2025-03-13 22:09:18 +0000

    refactor route url

--- dashboard/src/app/api/routes.ts
+++ dashboard/src/app/api/routes.ts
@@ -2,12 +2,16 @@ import { NextResponse } from "next/server"
 
 export async function GET() {
 
-    const res = await fetch("http://localhost:5000/api/students", {
+    const res = await fetch("http://localhost:5000/api/attendance", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
       },
     });
+
+    if (!res.ok) {
+        throw new Error("Failed to fetch students data");
+      }
 
     const students = await res.json();
 
