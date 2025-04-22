401a21e6a426faccf0308c9bced65a4f8072b04c
joela03 <joelallencaliste@gmail.com>
2025-04-22 22:32:02 +0100

    removed useless library calls

--- backend/functions.py
+++ backend/functions.py
@@ -11,10 +11,6 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
